0e4126f4dbcec1b3377beeb7176ce26c3229fce1
Kunal Jaykam <50500283+kunaljaykam@users.noreply.github.com>
2022-07-09 18:42:26 +0530

    Create clean-tomcat.sh

---
+++ clean-tomcat.sh
@@ -0,0 +1,8 @@
+# clean tomcat
+
+if [ -d /home/kunaljaykam/sakaiproject/apache-tomcat-9.0.64 ]; then
+    rm -rf /home/kunaljaykam/sakaiproject/apache-tomcat-9.0.64
+fi
+
+
+unzip /home/kunaljaykam/tomcat/apache-tomcat-9.0.64.zip
