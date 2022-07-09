1472597334843b5a9cc258da9eff4b45495ed857
Kunal Jaykam <50500283+kunaljaykam@users.noreply.github.com>
2022-07-09 18:46:23 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+mvn clean install -Dmaven.test.skip=true -T 4C -Dmaven.tomcat.home=/home/kunaljaykam/sakaiproject/apache-tomcat-9.0.64 -Dsakai.home=/home/kunaljaykam/sakaiproject/sakai-home sakai:deploy
